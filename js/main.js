f31451053584038f926ee7ef786d67343a167ce5
pedroj4567 <pedroj4567@gmail.com>
2022-04-16 21:45:09 -0400

    Subiendo archivos

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+
+const typed_1  = new Typed('.typed',{
+    strings:['https://www.WebDeveloper.com/$PedroAcosta', 'Crea.', 'Sueña.','Programa.'],
+    typeSpeed : 14,
+    cursorChar : '|',
+    showCursor : true ,  
+    autoInsertCss : true ,
+})
